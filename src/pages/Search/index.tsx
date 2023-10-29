56c720e3a28442069f971c877909b85e614eeb5a
Oleg Bendiucov <punisher8585@gmail.com>
2023-10-29 16:41:55 +0200

    feat: add search result

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,12 +1,18 @@
 import { Component } from "react";
 import SearchTop from "./Top";
+import SearchResults from "./Result";
 
 interface SearchPageProps {
   params: object;
 }
 
+interface SearchResult {
+  name: string;
+}
+
 interface SearchPageState {
   searchText: string;
+  results: SearchResult[],
   error: Error | null;
 }
 
@@ -15,12 +21,35 @@ class SearchPage extends Component<SearchPageProps, SearchPageState> {
     super(props);
     this.state = {
       searchText: '',
+      results: [],
       error: null,
     };
   }
 
   handleSearch = (newSearchText: string) => {
-    console.log(newSearchText);
+    const clearSearchText = newSearchText.trim();
+
+    fetch(`https://stapi.co/api/v1/rest/animal/search?name=${clearSearchText}`, 
+    {
+      method: "POST"
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(({animals}) => {
+        this.setState({
+          searchText: clearSearchText,
+          results: animals,
+          error: null,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        this.setState({ error });
+      });
   }
 
   render() {
@@ -30,6 +59,7 @@ class SearchPage extends Component<SearchPageProps, SearchPageState> {
           searchText={this.state.searchText} 
           onSearch={this.handleSearch}
         />
+        <SearchResults results={this.state.results} />
       </>
     );
   }
